6d7a95d05cbeeb561e4dcfabbe7c66ef3d1831e9
Fatapix <fatapix@gmail.com>
2017-12-05 23:54:21 +0100

    Integration CPS

--- src/main/java/fr/utaria/talos/Talos.java
+++ src/main/java/fr/utaria/talos/Talos.java
@@ -1,9 +1,33 @@
 package fr.utaria.talos;
 
 import fr.utaria.talos.modules.CPS;
+import fr.utaria.talos.modules.PlayerInfos;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+
 public class Talos extends JavaPlugin {
+
+	public static Talos instance;
+	public static ArrayList<PlayerInfos> players;
+
+	public static Talos getInstance() {
+		return instance;
+	}
+
+	public static ArrayList<PlayerInfos> getPlayers() {
+		return players;
+	}
+
+	public static PlayerInfos getPlayerInfos(Player p) {
+		for(PlayerInfos playerinfos : players) {
+			if(playerinfos.getPlayer().getUniqueId().equals(p.getUniqueId())) {
+				return playerinfos;
+			}
+		}
+		return null;
+	}
 
 	@Override
 	public void onEnable() {
